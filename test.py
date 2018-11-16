ca4b9ab9a256a1e8f6190cf76520983dbe8edc83
jlscience <julius_lange@web.de>
2018-11-16 23:13:54 +0100

    testing stuff

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 # import h5py
 # print(h5py.__version__)
 
-l = [3, 5, 9]
-print(str(l))
+# l = [3, 5, 9]
+# print(str(l))
 
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.loadtxt('train_output.txt')
+print(data.shape)
+x = np.arange(0, data.shape[0], 1)
+plt.plot(x, data[:, 1])
+plt.show()
