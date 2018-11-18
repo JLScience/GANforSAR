67985517bc2c292fa23c97b05c0498075504c0a2
jlscience <julius_lange@web.de>
2018-11-18 09:56:00 +0100

    edited split_image method such that the number of subimages taken per image can be specified; works with 0, 1, 2 and 4 where 0 leaves the function unchanged

--- augmentation.py
+++ augmentation.py
@@ -85,18 +85,39 @@ def lee_filter_dataset(dataset_sar_norm, window_size=3):
     return dataset_sar_filtered
 
 
-def split_images(dataset, factor):
+# implemented for factor in [2, 4] and num_images_per_split <= factor and in [0, 1, 2, 4]
+def split_images(dataset, factor, num_images_per_split=0):
     num = dataset.shape[0]
     width = dataset.shape[1]
     channels = dataset.shape[3]
     w = int(width / factor)
 
-    new_dset = np.zeros((num * factor**2, w, w, channels), dtype=dataset.dtype)
+    if num_images_per_split != 0:
+        new_dset = np.zeros((num * num_images_per_split, w, w, channels), dtype=dataset.dtype)
+    else:
+        new_dset = np.zeros((num * factor ** 2, w, w, channels), dtype=dataset.dtype)
+
+    ind_list = [0]
+    if num_images_per_split == 2:
+        if factor == 4:
+            ind_list = [0, 2]
+        else:
+            ind_list = [0, 1]
+    elif num_images_per_split == 4:
+        ind_list = [0, 1, 2, 3]
+
     counter = 0
     for i in range(factor):
         for j in range(factor):
-            new_dset[counter*num:(counter+1)*num, :, :, :] = dataset[:, i*w:(i+1)*w, j*w:(j+1)*w, :]
-            counter += 1
+            if num_images_per_split == 0:
+                new_dset[counter * num:(counter + 1) * num, :, :, :] = dataset[:, i * w:(i + 1) * w, j * w:(j + 1) * w,
+                                                                       :]
+                counter += 1
+            else:
+                if i == j and i in ind_list:
+                    new_dset[counter * num:(counter + 1) * num, :, :, :] = dataset[:, i * w:(i + 1) * w,
+                                                                           j * w:(j + 1) * w, :]
+                    counter += 1
 
     return new_dset
 
@@ -223,7 +244,7 @@ def test_lee_filter():
     # plt.show()
 
 
-def test_split_images():
+def test_split_images_old():
     import data_io
     import matplotlib.pyplot as plt
 
@@ -254,5 +275,28 @@ def test_split_images():
     plt.show()
 
 
+def test_split_images():
+    import matplotlib.pyplot as plt
+    import imageio
+    im1 = imageio.imread('data/EuroSAT/AnnualCrop/AnnualCrop_12.jpg')
+    im2 = imageio.imread('data/EuroSAT/AnnualCrop/AnnualCrop_13.jpg')
+    tensor = np.concatenate([im1.reshape((1, 64, 64, 3)), im2.reshape((1, 64, 64, 3))], axis=0)
+    print(tensor.shape)
+    tensor_small = split_images(tensor, 4, 4)
+    print(tensor_small.shape)
+    fig, axs = plt.subplots(2, 5)
+    axs[0, 0].imshow(tensor[0, ...])
+    axs[0, 1].imshow(tensor_small[0, ...])
+    axs[0, 2].imshow(tensor_small[2, ...])
+    axs[0, 3].imshow(tensor_small[4, ...])
+    axs[0, 4].imshow(tensor_small[6, ...])
+    axs[1, 0].imshow(tensor[1, ...])
+    axs[1, 1].imshow(tensor_small[1, ...])
+    axs[1, 2].imshow(tensor_small[3, ...])
+    axs[1, 3].imshow(tensor_small[5, ...])
+    axs[1, 4].imshow(tensor_small[7, ...])
+    plt.show()
+
 # - - - - - - - - - - - - - - -
 
+test_split_images()
--- test.py
+++ test.py
@@ -4,10 +4,51 @@
 # l = [3, 5, 9]
 # print(str(l))
 
-import numpy as np
-import matplotlib.pyplot as plt
-data = np.loadtxt('train_output.txt')
-print(data.shape)
-x = np.arange(0, data.shape[0], 1)
-plt.plot(x, data[:, 1])
-plt.show()
+# import numpy as np
+# import matplotlib.pyplot as plt
+# data = np.loadtxt('train_output.txt')
+# print(data.shape)
+# x = np.arange(0, data.shape[0], 1)
+# plt.plot(x, data[:, 1])
+# plt.show()
+
+# import numpy as np
+# num_images_per_split = 2
+# factor = 2
+# num = 10
+# width = 8
+# w = int(width / factor)
+# channels = 3
+#
+# dataset = np.arange(0, num*width*width*channels, 1).reshape((num, width, width, channels))
+#
+# if num_images_per_split != 0:
+#     new_dset = np.zeros((num * num_images_per_split, w, w, channels), dtype=dataset.dtype)
+# else:
+#     new_dset = np.zeros((num * factor**2, w, w, channels), dtype=dataset.dtype)
+#
+# ind_list = [0]
+# if num_images_per_split == 2:
+#     if factor == 4:
+#         ind_list = [0, 2]
+#     else:
+#         ind_list = [0, 1]
+# elif num_images_per_split == 4:
+#     ind_list = [0, 1, 2, 3]
+#
+# counter = 0
+# for i in range(factor):
+#     for j in range(factor):
+#         if num_images_per_split == 0:
+#             new_dset[counter*num:(counter+1)*num, :, :, :] = dataset[:, i*w:(i+1)*w, j*w:(j+1)*w, :]
+#             counter += 1
+#         else:
+#             if i == j and i in ind_list:
+#                 new_dset[counter*num:(counter+1)*num, :, :, :] = dataset[:, i*w:(i+1)*w, j*w:(j+1)*w, :]
+#                 counter += 1
+# print(dataset[0, :, :, 0])
+# print(new_dset[0, :, :, 0])
+# print(new_dset[10, :, :, 0])
+# # print(new_dset[20, :, :, 0])
+# # print(new_dset[30, :, :, 0])
+# print(new_dset.shape)
