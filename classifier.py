4b4348ef0dfc8df2d31c918f0fa4211708fce87f
jlscience <julius_lange@web.de>
2018-11-20 09:39:50 +0100

    prepared vgg16 class; training method left open

--- classifier.py
+++ classifier.py
@@ -1,14 +1,57 @@
 #
 import numpy as np
 import keras
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense
+from keras.optimizers import Adam
 
-vgg16_model = keras.applications.vgg16.VGG16(include_top=False, input_shape=(64, 64, 3))
-# resnet_model = keras.applications.resnet50.ResNet50(include_top=False, input_shape=(64, 64, 3))
-# mobilenet_model = keras.applications.mobilenet.MobileNet(include_top=False)
+MODEL_WEIGHTS_PATH = ''
 
-vgg16_model.summary()
-# resnet_model.summary()
-# mobilenet_model.summary()
 
-# x = np.random.rand(1, 64, 64, 3)
-# y = mobilenet_model.predict(x)
+class Custom_VGG16():
+
+    def __init__(self):
+        self.input_shape = (64, 64, 3)
+        self.classifier = self.build_model()
+        self.classifier.summary()
+
+    def build_model(self):
+        vgg16_model = keras.applications.vgg16.VGG16(include_top=False, input_shape=(64, 64, 3))
+        vgg16_model.trainable = False
+        inp = Input(shape=(64, 64, 3))
+        outp = vgg16_model(inp)
+        outp = Flatten()(outp)
+        outp = Dense(512, activation='relu')(outp)
+        outp = Dense(512, activation='relu')(outp)
+        outp = Dense(10, activation='softmax')(outp)
+        return Model(inp, outp)
+
+    def train(self):
+        opt = Adam()
+        self.classifier.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+        # TODO: load datasets
+
+        # TODO: preprocess datasets
+
+        # TODO: train classifier
+
+        # TODO: save weights (in between)
+
+
+    def save_trained_model(self):
+        self.classifier.save_weights(MODEL_WEIGHTS_PATH)
+
+    def load_trained_model(self):
+        self.classifier.load_weights(MODEL_WEIGHTS_PATH)
+
+    def apply(self, batch):
+        return self.classifier.predict(batch)
+
+
+if __name__ == '__main__':
+    my_vgg = Custom_VGG16()
+    my_vgg.train()
+    # x = np.random.rand(12, 64, 64, 3)
+    # y = my_vgg.apply(x)
+    # print(y.shape)
