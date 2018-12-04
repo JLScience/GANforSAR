38d117c7da4fb258393f840e8f8feca0b9505d1e
jlscience <julius_lange@web.de>
2018-12-04 19:45:44 +0100

    implemented the discriminator

--- sen12_esrgan.py
+++ sen12_esrgan.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import keras
 from keras.models import Model
-from keras.layers import Conv2D, LeakyReLU, Concatenate, Lambda, Add, Input
+from keras.layers import Conv2D, LeakyReLU, Concatenate, Lambda, Dense, Add, Input, BatchNormalization, Flatten
 
 # own packages:
 import data_io
@@ -30,17 +30,21 @@ class ESRGAN():
         self.img_channels = 3
         self.img_shape = (self.img_rows, self.img_cols, self.img_channels)
 
-        self.num_f = 32
+        self.num_f_g = 32
+        self.num_f_d = 64
         self.f_size = 3
         self.num_rrdbs = 16
 
         self.generator = self.make_generator()
-        self.generator.summary()
+        # self.generator.summary()
+
+        self.discriminator = self.make_discriminator()
+        self.discriminator.summary()
 
     def make_generator(self):
 
         # no activation by default
-        def conv_lrelu(inp, filters, f_size, use_act=False, alpha=0):
+        def conv_lrelu(inp, filters, f_size, use_act=False, alpha=0.0):
             outp = Conv2D(filters, kernel_size=f_size, strides=1, padding='same')(inp)
             if use_act:
                 outp = LeakyReLU(alpha=alpha)(outp)
@@ -70,25 +74,45 @@ class ESRGAN():
 
         inp = Input(shape=self.img_shape)
         # first convolution (no activation function):
-        outp = conv_lrelu(inp, filters=self.num_f, f_size=self.f_size)
+        outp = conv_lrelu(inp, filters=self.num_f_g, f_size=self.f_size)
         # save output of first convolution to add it to the output of the RRDB's:
         out1 = outp
         # RRDB's:
         for _ in range(self.num_rrdbs):
-            outp = rrd_block(outp, filters=self.num_f, f_size=self.f_size, alpha=0.2, residual_scaling_factor=0.2)
+            outp = rrd_block(outp, filters=self.num_f_g, f_size=self.f_size, alpha=0.2, residual_scaling_factor=0.2)
         # convolution after RRDB's (no activation function):
-        outp = conv_lrelu(outp, filters=self.num_f, f_size=self.f_size)
+        outp = conv_lrelu(outp, filters=self.num_f_g, f_size=self.f_size)
         # Add output of convolution before and after RRDB's:
         outp = Add()([out1, outp])
         # here upsampling would normally take place
         # ...
         # two convolutions at the end; the first with activation, the second without (why no tanh?)
-        outp = conv_lrelu(outp, filters=self.num_f, f_size=self.f_size, use_act=True, alpha=0.2)
+        outp = conv_lrelu(outp, filters=self.num_f_g, f_size=self.f_size, use_act=True, alpha=0.2)
         outp = conv_lrelu(outp, filters=self.img_channels, f_size=self.f_size)
         return Model(inp, outp)
 
     def make_discriminator(self):
-        pass
+        def conv_block(inp, filters, f_size, strides, alpha, use_bn=True):
+            outp = Conv2D(filters, kernel_size=f_size, strides=strides, padding='same')(inp)
+            if use_bn:
+                outp = BatchNormalization()(outp)
+            outp = LeakyReLU(alpha)(outp)
+            return outp
+
+        inp = Input(shape=self.img_shape)
+        outp = conv_block(inp, self.num_f_d, self.f_size, strides=1, alpha=0.2, use_bn=False)
+        outp = conv_block(outp, self.num_f_d, self.f_size, strides=2, alpha=0.2)
+        outp = conv_block(outp, self.num_f_d * 2, self.f_size, strides=1, alpha=0.2)
+        outp = conv_block(outp, self.num_f_d * 2, self.f_size, strides=2, alpha=0.2)
+        outp = conv_block(outp, self.num_f_d * 4, self.f_size, strides=1, alpha=0.2)
+        outp = conv_block(outp, self.num_f_d * 4, self.f_size, strides=2, alpha=0.2)
+        outp = conv_block(outp, self.num_f_d * 8, self.f_size, strides=1, alpha=0.2)
+        outp = conv_block(outp, self.num_f_d * 8, self.f_size, strides=2, alpha=0.2)
+        outp = Flatten()(outp)
+        outp = Dense(1024)(outp)
+        outp = LeakyReLU(0.2)(outp)
+        outp = Dense(1, activation='sigmoid')(outp)
+        return Model(inp, outp)
 
     def train(self):
         pass
